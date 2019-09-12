ada7332a825bbed81fc8c6e04ccaf2a32c4cc482
maddy <mecha@andrew.cmu.edu>
2019-09-12 11:35:43 -0400

    aaaa

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var slideIndex = 1;
-showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -26,8 +25,60 @@ function showSlides(n) {
 }
 
 
-function enter(){
-    document.getElementById("popup").style.display = "none";
-        console.log(poppy);
+
+
+var slideIndex2 = 1;
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
 }
 
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";  
+  }
+  for (i = 0; i < dots2.length; i++) {
+      dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";  
+  dots2[slideIndex2-1].className += " active";
+}
+
+
+
+
+
+var slideIndex3 = 1;
+
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+function currentSlide3(n) {
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+  var i;
+  var slides3 = document.getElementsByClassName("mySlides3");
+  var dots3 = document.getElementsByClassName("dot3");
+  if (n > slides3.length) {slideIndex3 = 1}    
+  if (n < 1) {slideIndex3 = slides3.length}
+  for (i = 0; i < slides3.length; i++) {
+      slides3[i].style.display = "none";  
+  }
+  for (i = 0; i < dots3.length; i++) {
+      dots3[i].className = dots3[i].className.replace(" active", "");
+  }
+  slides3[slideIndex3-1].style.display = "block";  
+  dots3[slideIndex3-1].className += " active";
+}
